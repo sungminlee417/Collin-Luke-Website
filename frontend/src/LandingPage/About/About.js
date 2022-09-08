e2123f34038b1cbef03d1189cebdcd89c72f213c
Hannah Lee <hannahlee@Hannahs-MacBook-Air.local>
2022-09-08 17:25:28 -0500

    adding hero and about

---
+++ frontend/src/LandingPage/About/About.js
@@ -0,0 +1,12 @@
+import "./About.css";
+const About = () => {
+    return (
+        <section id="about-page">
+            <header id="about-page-header">
+                About
+            </header>
+        </section>
+    )
+}
+
+export default About;
--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
+import About from "./About";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <section id="landing-page">
+      <div id="hero-container">
       <header id="landing-page-header">
         Collin Holloway and Luke Benedict
-      </header>
+      </header> 
+      </div>
+      <About/>
     </section>
   );
 };

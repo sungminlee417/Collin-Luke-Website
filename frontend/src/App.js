3e19a36a07a4b186648787215b38f388a5a00d59
Sungmin Lee <sungminlee417@gmail.com>
2022-09-08 14:46:37 -0700

    asdf

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import LandingPage from "./LandingPage";
+
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
---
+++ frontend/src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <section id="landing-page">
+      <header id="landing-page-header">
+        Collin Holloway and Luke Benedict
+      </header>
+    </section>
+  );
+};
+
+export default LandingPage;

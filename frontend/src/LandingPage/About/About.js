9e4feb34d67b84d5279b8b7e1ffa1b066f8e7980
Hannah Lee <hannahlee@Hannahs-MacBook-Air.local>
2022-09-10 12:37:53 -0500

    Editing about page

--- frontend/src/LandingPage/About/About.js
+++ frontend/src/LandingPage/About/About.js
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <section id="about-section">
       <header id="about-section-header">(1) About</header>
-      <h1 id="about-large-text">Lorem ipsum dolor sit</h1> 
-      <h2 id="about-small-text">amet consectetur adipisicing elit. Culpa repellendus totam consequatur sed labore at ipsum dolorem commodi sequi cum! Error minima quam suscipit id placeat fuga, ipsa commodi repellat?</h2>
-      <div id="box-behind-duo"></div>
-      <div class="about-image-duo">
-        <img src= {image} alt="Collin and Luke"></img>
+      <div className="about-duo-section">
+        <h1 id="about-large-text">Lorem ipsum dolor sit</h1> 
+        <h2 id="about-small-text">amet consectetur adipisicing elit. Culpa repellendus totam consequatur sed labore at ipsum dolorem commodi sequi cum! Error minima quam suscipit id placeat fuga, ipsa commodi repellat?</h2>
+        <div id="box-behind-duo"></div>
+        <img className="about-image-duo" src={image} alt="Collin and Luke"></img>
       </div>
     </section>
   );

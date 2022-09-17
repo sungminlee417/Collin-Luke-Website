2d562815104dcc656b1ebc855980d8f428d3e640
Sungmin Lee <sungminlee417@gmail.com>
2022-09-17 11:02:53 -0700

    fixed recordings

--- frontend/src/Recordings/Recordings.js
+++ frontend/src/Recordings/Recordings.js
@@ -16,67 +16,22 @@ const recordingsURL = [recordingOne, recordingTwo];
 
 const Recordings = () => {
   const [current, setCurrent] = useState(null);
-  const [resize, setResize] = useState(false);
 
   const onPlay = (recordingId) => {
     setCurrent(recordingId);
-    setResize(true);
-    const currentRecording = document.querySelector(
-      `.recordings-video-container-${recordingId}`
-    );
-    if (!currentRecording.classList.contains("enlarge-video")) {
-      currentRecording.classList.add("enlarge-video");
-    }
-    if (currentRecording.classList.contains("shrink-video")) {
-      currentRecording.classList.remove("shrink-video");
-    }
-
-    recordingsURL.forEach((recording) => {
-      if (recordingId !== recording.id) {
-        const pausedRecording = document.querySelector(
-          `.recordings-video-container-${recording.id}`
-        );
-        pausedRecording.classList.add("shrink-video");
-      }
-    });
-  };
-
-  if (current) {
-    const button = document.querySelector(".recordings-resize-button");
-    button.classList.add("show");
-  }
-
-  const resizeRecordings = () => {
-    setCurrent(null);
-    recordingsURL.forEach((recordingData) => {
-      const recording = document.querySelector(
-        `.recordings-video-container-${recordingData.id}`
-      );
-      recording.classList.remove("shrink-video");
-      recording.classList.remove("enlarge-video");
-    });
-    const button = document.querySelector(".recordings-resize-button");
-    button.classList.remove("show");
   };
 
   return (
     <section className="recordings-section content-margin">
       <h3>Recordings</h3>
-      <button className={`recordings-resize-button`} onClick={resizeRecordings}>
-        <span className="recordings-line recordings-line-one"></span>
-        <span className="recordings-line recordings-line-two"></span>
-      </button>
       <ul className="recordings-videos-container">
         {recordingsURL.map((recording, index) => {
           return (
-            <div
-              key={index}
-              className={`recordings-video-container recordings-video-container-${recording.id}`}
-            >
+            <div key={index} className={`recordings-video-container`}>
               <ReactPlayer
                 url={recording.url}
-                width="100%"
-                height="100%"
+                width="560"
+                height="315"
                 controls
                 playing={current === recording.id ? true : false}
                 className={`recordings-youtube-video recordings-youtube-video-${recording.id}`}

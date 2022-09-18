daf54c69ecee5a4c413f3521381ef0b45c998817
Sungmin Lee <sungminlee417@gmail.com>
2022-09-18 09:23:51 -0700

    Asdf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import Div100vh from "react-div-100vh";
 import Navigation from "./Navigation";
 import Hero from "./Hero";
 import About from "./About";
@@ -6,15 +7,17 @@ import Photos from "./Photos";
 import Contact from "./Contact";
 
 function App() {
+  window.scrollTo(0, 1);
+
   return (
-    <>
+    <Div100vh>
       <Navigation />
       <Hero />
       <About />
       <Recordings />
       <Photos />
       <Contact />
-    </>
+    </Div100vh>
   );
 }
 

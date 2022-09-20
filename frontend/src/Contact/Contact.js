a72a43e4f0ccb0dd4595e42994601229cec17973
Hannah Lee <hannahlee@Hannahs-MacBook-Air.local>
2022-09-19 20:14:55 -0500

    final changes

--- frontend/src/Contact/Contact.js
+++ frontend/src/Contact/Contact.js
@@ -112,23 +112,4 @@ export const Contact = () => {
   );
 };
 
-// const Contact = () => {
-//     const [email,setEmail] = useState("")
-//     const onSubmit = (e) => {
-//         e.preventDefault()
-//         console.log(email)
-//     }
-//     return (
-//         <section id="contact-section">
-//             <form onSubmit={onSubmit}>
-//                 <div className="email-container">
-//                 <label htmlFor="contact-email">Email</label>
-//                 <input placeholder="Enter Email here" name="contact-email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-//                 </div>
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </section>
-//     );
-// };
-
 export default Contact;

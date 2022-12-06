c3a8d8c9b0b67079eaf397d38f8567d8e5c253d3
Sungmin Lee <sungminlee417@gmail.com>
2022-12-05 21:44:18 -0800

    added bio

--- frontend/src/About/About.js
+++ frontend/src/About/About.js
@@ -10,12 +10,35 @@ const About = () => {
         <div className="about-duo-section">
           <h2 id="about-large-text">The Muse Duo </h2>
           <h3 id="about-small-text">
-            is a guitar and piano duo comprised of the guitarist Collin Holloway
-            and composer/pianist Robert Luke Benedict. The Duo champions new
-            chamber music for this rare setting of instruments through Luke's
-            compositions, as well as the Duo's premiere performances of these
-            works. Through music that is both experimental and accessible, the
-            Muse Duo brings a new musical perspective to chamber music.{" "}
+            <div>
+              Originally formed at the Eastman School of Music, the MUSE DUO is
+              a one-of-a-kind ensemble in the world of classical music.
+              Comprised of the award-winning guitarist Collin Holloway and the
+              dynamic pianist/composer Luke Benedict, the guitar and piano duo
+              brings music that is eclectic and accessible to modern audiences.
+            </div>
+            <div>
+              Just months after playing together for the first time, the duo
+              embarked on a tour throughout the United States. Since then, the
+              Muse Duo has performed in various venues and festivals, including
+              a tour of the midwest United States, the “Lynn New Music Festival”
+              and “Shellpoint Young Artist Series”. In July of 2022, the Duo
+              traveled to New York to record their debut album, consisting of
+              works composed by Luke, which will be released in the Spring of
+              2023.
+            </div>
+            <div>
+              The MUSE DUO strives to champion new compositions for the unique
+              ensemble. Their repertoire primarily consists of Luke’s original
+              works, where he crafts experiences tailored especially for the
+              duo. These works are melodically focused and neo-classically
+              inspired, while engaging audiences through it’s modernistic
+              rhythmic drive.
+            </div>
+            <div>
+              Through music that is both experimental and accessible, the Muse
+              Duo brings a new perspective to chamber music.
+            </div>
           </h3>
         </div>
       </div>

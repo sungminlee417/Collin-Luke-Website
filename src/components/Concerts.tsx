eb6a6c7d93730fcc232f2197cfffd6f101713540
Sungmin Lee <sungminlee417@gmail.com>
2023-03-16 15:05:16 -0500

    added text decoration for address

--- src/components/Concerts.tsx
+++ src/components/Concerts.tsx
@@ -125,7 +125,7 @@ const Concerts = () => {
                   <div className="flex md:gap-2 gap-4 items-center">
                     <i className="fa-solid fa-location-dot"></i>
                     <div
-                      className="text-xl"
+                      className="text-xl underline decoration-dotted underline-offset-2"
                       onClick={() =>
                         openMap(
                           "https://www.google.com/maps/place/Sunshine+Cathedral+MCC/@26.1033971,-80.1550231,17z/data=!3m2!4b1!5s0x88d9008d6d986b25:0xf8a9f6d227cc987b!4m6!3m5!1s0x88d9008d0d6d3729:0xc7337a9ff83af3d9!8m2!3d26.1033923!4d-80.1528344!16s%2Fg%2F113hfcjh6"

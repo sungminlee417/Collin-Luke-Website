f84183fb3e189960b878938ddb348e1701e80bf5
Sungmin Lee <sungminlee417@gmail.com>
2023-03-16 14:38:46 -0500

    fixed grid flexbox styling for recordings

--- src/components/Recordings.tsx
+++ src/components/Recordings.tsx
@@ -26,7 +26,7 @@ const Recordings = () => {
   return (
     <section className="recordings-section flex flex-col gap-14 md:m-20 m-12">
       <h3>Recordings</h3>
-      <ul className="md:grid grid-cols-2 flex flex-col lg:gap-16 gap-10 mx-0">
+      <ul className="lg:grid grid-cols-2 flex flex-col lg:gap-16 gap-10 mx-0">
         {recordingsURL.map((recording, index) => {
           return (
             <div key={recording.id}>

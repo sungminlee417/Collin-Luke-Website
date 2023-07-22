0543408af46760ee602d1d643dfd10e9a3dc12a3
Sungmin Lee <sungminlee417@gmail.com>
2023-07-21 21:12:37 -0700

    changed time on concert data

--- src/data/concerts.ts
+++ src/data/concerts.ts
@@ -1,25 +1,11 @@
 const concerts = [
   {
     startDate: {
-      day: 6,
-      month: "July",
-    },
-    endDate: {
-      day: 9,
-      month: "July",
-    },
-    venue: "Off The Dock Chamber Festival",
-    location:
-      "First Presbyterian Church of Skaneateles - 97 East Genesee Street Skaneateles, NY 13152",
-    moreInfoUrl: "https://offthedockchamber.com/",
-  },
-  {
-    startDate: {
       day: 23,
       month: "July",
     },
     venue: "St. Mary's Episcopal Church",
-    time: "TBA",
+    time: "3:00PM",
     location: "623 E Ocean Blvd, Stuart, FL, 34994",
     moreInfoUrl: "https://stmarys-stuart.org/",
   },

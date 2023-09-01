292459d5a7f5069d0eb7d9ce8855c8dd2d9e5045
Sungmin Lee <sungminlee417@gmail.com>
2023-09-01 09:22:54 -0700

    changed contact form to a container to add space

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,7 +17,7 @@ export const Contact = () => {
   };
 
   return (
-    <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+    <section className="relative flex flex-wrap lg:h-screen lg:items-center container mx-auto">
       <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
         <div className="mx-auto max-w-lg text-center">
           <h1 className="text-2xl font-bold sm:text-3xl">Connect with us!</h1>

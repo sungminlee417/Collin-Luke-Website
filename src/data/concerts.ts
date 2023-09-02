3972c9f6e847145d567c5e4479101e208e400ea4
Sungmin Lee <sungminlee417@gmail.com>
2023-09-02 16:49:54 -0700

    added in past concert dates

--- src/data/concerts.ts
+++ src/data/concerts.ts
@@ -1,6 +1,86 @@
 const concerts = [
   {
     startDate: {
+      day: 7,
+      month: "May",
+      year: 2023,
+    },
+    venue: "Schmidt Vocal Arts Center",
+    time: "3:00PM EDT",
+    location: "412 Rose St, Lexington, KY 40508 Kentucky, KY 40508",
+    ticketUrl:
+      "https://www.eventbrite.com/e/the-muse-duo-experiments-2023-album-release-tour-tickets-597129489377",
+  },
+  {
+    startDate: {
+      day: 20,
+      month: "May",
+      year: 2023,
+    },
+    venue: "Sunshine Cathedral Center for the Performing Arts",
+    time: "8:00PM EDT",
+    location: "1480 SW 9th Ave Fort Lauderdale, FL 33315",
+    ticketUrl:
+      "https://www.eventbrite.com/e/sunshine-cathedral-center-for-the-performing-arts-presents-muse-duo-tickets-538604740277?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=escb",
+  },
+  {
+    startDate: {
+      day: 26,
+      month: "May",
+      year: 2023,
+    },
+    venue: "ARTSPACE at Untitled",
+    time: "3:00PM CDT",
+    location: "1 NE 3rd St, Oklahoma City, OK 73104",
+    ticketUrl:
+      "https://www.eventbrite.com/e/the-muse-duo-experiments-2023-album-release-tour-tickets-597032589547",
+  },
+  {
+    startDate: {
+      day: 28,
+      month: "May",
+      year: 2023,
+    },
+    venue: "First Presbyterian Church",
+    time: "3:00PM CDT",
+    location: "1001 NW 25th St, Oklahoma City, OK 73106, United States",
+    ticketUrl:
+      "https://www.eventbrite.com/e/the-muse-duo-experiments-2023-album-release-tour-tickets-597247522417",
+  },
+  {
+    startDate: {
+      day: 25,
+      month: "June",
+      year: 2023,
+    },
+    venue: "Boca Museum of Art",
+    time: "8:00PM EDT",
+    location: "501 Plaza Real, Boca Raton, FL 33432, United States",
+    ticketUrl: "https://bocamuseum.org/visit/events",
+  },
+  {
+    startDate: {
+      day: 6,
+      month: "July",
+      year: 2023,
+    },
+    venue: "Off The Dock Chamber Festival",
+    location: "623 E Ocean Blvd, Stuart, FL 34996, United States",
+    moreInfoUrl: "https://offthedockchamber.com/",
+  },
+  {
+    startDate: {
+      day: 23,
+      month: "July",
+      year: 2023,
+    },
+    venue: "St. Mary's Episcopal Church",
+    time: "TBA",
+    location: "623 E Ocean Blvd, Stuart, FL, 34994",
+    moreInfoUrl: "https://stmarys-stuart.org/",
+  },
+  {
+    startDate: {
       day: 14,
       month: "October",
       year: 2023,

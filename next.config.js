8a1f07600c3e06e335fc316fc18d9257c475ece5
Sungmin Lee <sungminlee417@gmail.com>
2025-08-14 21:17:03 -0700

    fix: remove netlify toml

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   trailingSlash: true,
   
   // Performance optimizations
@@ -17,7 +16,6 @@ const nextConfig = {
   // ...(process.env.ANALYZE === 'true' && { bundleAnalyzer: { enabled: true } }),
   
   images: {
-    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
